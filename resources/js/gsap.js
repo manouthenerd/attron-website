2ad193d5c4a27177c48bc2ad9941ba00f1b94500
Leplusbio225 <196662423+manouthenerd@users.noreply.github.com>
2025-09-15 14:40:49 +0000

    Suppression des animations GSAP inutilisées pour optimiser le code

--- resources/js/gsap.js
+++ resources/js/gsap.js
@@ -14,59 +14,7 @@ window.addEventListener("load", (e) => {
         gsap.to(window, { duration: 0.5, scrollTo: { y: 0 }});
     })
     
-
-        gsap.from('#circle', {
-            scrollTrigger: {
-                trigger: '#circle',
-                
-            },
-            
-            y: 50,
-            opacity: 0
-        })
     
-        gsap.from('.box', {
-            scrollTrigger: {
-                trigger: '.box',
-            },
-    
-            xPercent: 100,
-            opacity: 0,
-            stagger: 0.5,
-            duration: 1.8
-    
-        })
-    
-    
-
-    gsap.from('.card', {
-        scrollTrigger: {
-            trigger: '.card',
-        },
-        opacity: 0,
-        rotationY: 30,
-        duration: 0.7,
-        stagger: 0.2
-    })
-
-    gsap.from('.image', {
-        scrollTrigger: {
-            trigger: '.image',
-            start: '-70% top',
-        },
-        rotationY: 360,
-        duration: 0.8,
-        stagger: 0.2
-    })
-
-    gsap.from('.picture', {
-        scrollTrigger: {
-            trigger: '.picture',
-        },
-        scale: 0,
-        duration: 0.8,
-        stagger: 0.2
-    })
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
